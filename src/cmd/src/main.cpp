a9fbca4d0ba0e863dd1ae4fdcc41b219e2af5ee2
hietwll <hietwll@gmail.com>
2021-10-25 08:37:02 +0800

    WIP: debug cornell box.

--- src/cmd/src/main.cpp
+++ src/cmd/src/main.cpp
@@ -24,35 +24,35 @@ int main()
     // camera
     real aspect_ratio = 1024_r / 768.0_r;
     int width = 1024;
-    SP<const Camera> camera = CreatePinPoleCamera(vec3 (50.0_r, 52.0_r, 295.6_r),
-                                                  glm::normalize(vec3(0.0_r, -0.042612_r, -1.0_r)),
-                                            vec3(0.0, 1.0, 0.0_r),
-                                            1.0_r, DegToRad(54.0_r), aspect_ratio);
+    SP<const Camera> camera = CreatePinPoleCamera(vec3 (0.0_r, 8.0_r, 0.0_r),
+                                                  vec3(0.0_r, -1.0_r, 0.0_r),
+                                            vec3(0.0, 0.0, 1.0_r),
+                                            1.0_r, DegToRad(45.0_r), aspect_ratio);
 
     // objects
     std::vector<SP<const RenderObject>> objects;
 
     // left
-    auto left = CreateObj(1e5_r, vec3(1e5_r + 1.0_r, 40.8_r, 81.6_r), vec3(0.75_r, 0.25_r, 0.25_r));
-    auto right = CreateObj(1e5_r, vec3(-1e5_r + 99.0_r, 40.8_r, 81.6_r), vec3(0.25_r, 0.75_r, 0.75_r));
-    auto back = CreateObj(1e5_r, vec3(50.0_r, 40.8_r, 1e5_r), vec3(0.75_r, 0.75_r, 0.75_r));
-    auto front = CreateObj(1e5_r, vec3(50.0_r,  40.8_r, -1e5_r + 170.0_r), vec3(0.0_r, 0.0_r, 0.0_r));
-    auto bottom = CreateObj(1e5_r, vec3(50.0_r, 1e5_r, 81.6_r), vec3(0.75_r, 0.75_r, 0.75_r));
-    auto top = CreateObj(1e5_r, vec3(50.0_r, -1e5_r + 81.6_r, 81.6_r), vec3(0.75_r, 0.75_r, 0.75_r));
+    auto left = CreateObj(1e5_r, vec3(-1e5_r - 2.0_r, 0.0_r, 0.0_r), vec3(0.75_r, 0.25_r, 0.25_r));
+    auto right = CreateObj(1e5_r, vec3(1e5_r + 2.0_r, 0.0_r, 0.0_r), vec3(0.25_r, 0.75_r, 0.75_r));
+    auto back = CreateObj(1e5_r, vec3(0.0_r, -1e5_r - 2.0_r, 0.0_r), vec3(0.75_r, 0.75_r, 0.75_r));
+    auto front = CreateObj(1e5_r, vec3(0.0_r, 1e5_r + 2.0_r, 0.0_r), vec3(0.0_r, 0.0_r, 0.0_r));
+    auto bottom = CreateObj(1e5_r, vec3(0.0_r, 0.0_r, -1e5_r - 2.0_r), vec3(0.75_r, 0.75_r, 0.75_r));
+    auto top = CreateObj(1e5_r, vec3(0.0_r, 0.0_r, 1e5_r + 2.0_r), vec3(0.75_r, 0.75_r, 0.75_r));
 
-    auto mid_a = CreateObj(16.5_r, vec3(27.0_r, 16.5_r, 47.0_r), white);
-    auto mid_b = CreateObj(16.5_r, vec3(73.0_r, 16.5_r, 78.0_r), red);
+    auto mid_a = CreateObj(0.5_r, vec3(-1.0_r, -1.0_r, 1.5_r), white);
+    auto mid_b = CreateObj(0.5_r, vec3(0.5_r, 0.25_r, 1.5_r), red);
 
-    auto light = CreateObj(600.0_r, vec3(50.0_r, 681.6_r - 0.27_r, 81.6_r), white, white);
+    auto light = CreateObj(2.0_r, vec3(0.0_r, 0.0_r, -3.732_r), white, white * 15.0_r);
 
     objects.push_back(left);
     objects.push_back(right);
     objects.push_back(back);
-    objects.push_back(front);
+//    objects.push_back(front);
     objects.push_back(bottom);
     objects.push_back(top);
-//    objects.push_back(mid_a);
-//    objects.push_back(mid_b);
+    objects.push_back(mid_a);
+    objects.push_back(mid_b);
     objects.push_back(light);
 
     SP<const Aggregate> aggregate = CreateSimpleAggregate(objects);
